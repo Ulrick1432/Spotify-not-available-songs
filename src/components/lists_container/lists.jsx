aafccfd9088b342b6c671e9b597d695ae32b77be
TheaPC <ulrick.preben.hansen@live.dk>
2024-09-15 12:31:13 +0200

    Plan comment added

--- src/components/lists_container/lists.jsx
+++ src/components/lists_container/lists.jsx
@@ -1,5 +1,22 @@
 import './lists.css';
 
+/* Når der klikkes på et at playlisterne skal den gå i gennem alle "Songs" og retuner i anden liste
+   Hvilke song der ikke længere er tilgænge lig i users region. 
+   
+  Hvordan gøre dette? 
+  https://developer.spotify.com/documentation/web-api/reference/get-playlists-tracks
+
+  response af overnstående for at se, hvilke lande sangen er tilgængelig i. 
+  response.items[].track.album.available_markets = return fx ["CA", "BR", "IT"]
+  Hvis "DK" ikke er tilgængelig i array'en er det ikke en sang der er tilgængelig i Danmark. 
+  det samlet resultat kan gemmes i localStorage så daten kan indsætte fra App.jsx til Lists -
+  over sange for den klikkede playlist der IKKE er tilgænge i Danmark.
+
+  Måske skal man have liked songs via en anden Spotify API.
+  https://developer.spotify.com/documentation/web-api/reference/get-users-saved-tracks
+  
+*/
+
 const Lists = ({ headerText, nameOfItem, data = null }) => {
   return (
     <div className='container'>
